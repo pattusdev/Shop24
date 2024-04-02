768a6e28a3a0f920eca1bfa4167e9dd811cb8d91
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-02 10:30:40 +0200

    Create Cargo.java

---
+++ src/main/java/com/Bk24Shop/Shop/Entity/Cargo.java
@@ -0,0 +1,17 @@
+package com.Bk24Shop.Shop.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cargo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+
+}

9592ba01d6f9e01622a79f34a13c170ba9448f42
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-02 10:30:27 +0200

    Create CargoRepository.java

---
+++ src/main/java/com/Bk24Shop/Shop/Repository/CargoRepository.java
@@ -0,0 +1,8 @@
+package com.Bk24Shop.Shop.Repository;
+
+import com.Bk24Shop.Shop.Entity.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+
+}

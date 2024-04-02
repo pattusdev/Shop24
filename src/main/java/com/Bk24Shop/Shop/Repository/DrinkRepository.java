a3b1745ad6500925168020b746c635dd77060127
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-02 10:30:22 +0200

    Create DrinkRepository.java

---
+++ src/main/java/com/Bk24Shop/Shop/Repository/DrinkRepository.java
@@ -0,0 +1,8 @@
+package com.Bk24Shop.Shop.Repository;
+
+import com.Bk24Shop.Shop.Entity.Drink;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrinkRepository extends JpaRepository<Drink, Long> {
+
+}

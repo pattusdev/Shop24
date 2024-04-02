b6c175f88f3b5f076a39a4c4d15dfcdb4d1663b5
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-02 10:30:17 +0200

    Create ReceiptRepository.java

---
+++ src/main/java/com/Bk24Shop/Shop/Repository/ReceiptRepository.java
@@ -0,0 +1,8 @@
+package com.Bk24Shop.Shop.Repository;
+
+import com.Bk24Shop.Shop.Entity.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+
+}

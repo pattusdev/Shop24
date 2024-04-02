c91892745fe1d4e1e571617766a1af2755f8a7fb
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-02 10:30:25 +0200

    Create ClientRepository.java

---
+++ src/main/java/com/Bk24Shop/Shop/Repository/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.Bk24Shop.Shop.Repository;
+
+import com.Bk24Shop.Shop.Entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}

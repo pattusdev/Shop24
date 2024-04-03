5f5bd1763580c44e1dff4dda418a3d5757ed7ab7
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-03 09:05:34 +0200

    Update OrderRepository.java

--- src/main/java/com/Bk24Shop/Shop/Repository/OrderRepository.java
+++ src/main/java/com/Bk24Shop/Shop/Repository/OrderRepository.java
@@ -7,25 +7,25 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-        @Query(value = "SELECT DISTINCT o.order_id, o.client_id, o.order_date " +
-                "FROM bk.order o " +
-                "WHERE o.order_id IN ( " +
-                "    SELECT order_id " +
+        @Query(value = "SELECT DISTINCT o.id, o.client_id, o.timestamp " +
+                "FROM shop24.order o " +
+                "WHERE o.id IN ( " +
+                "    SELECT id " +
                 "    FROM ( " +
-                "        SELECT order_id, ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY order_date DESC) AS row_num " +
-                "        FROM bk.order " +
+                "        SELECT id, ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY timestamp DESC) AS row_num " +
+                "        FROM shop24.order " +
                 "    ) AS temp " +
                 "    WHERE row_num = 1 " +
                 ") " +
-                "ORDER BY o.order_date DESC " +
+                "ORDER BY o.timestamp DESC " +
                 "LIMIT 5", nativeQuery = true)
         List<Object[]> findTopFiveOrdersByDifferentClients();
 
-        @Query("SELECT o.orderId, c.name AS clientName " +
+        @Query("SELECT o.id, c.name AS clientName " +
                 "FROM Order o " +
                 "JOIN o.client c " +
                 "WHERE o.status = 'Completed' " +
-                "ORDER BY o.orderDate DESC " +
+                "ORDER BY o.timestamp DESC " +
                 "LIMIT 10")
         List<Object[]> findTopTenPaidOrdersWithDetails();
 

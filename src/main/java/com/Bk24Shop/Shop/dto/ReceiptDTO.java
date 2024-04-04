219fbafc596c4c1ab1439fbdc5618a20679442ac
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-04 11:30:02 +0200

    Create ReceiptDTO.java

---
+++ src/main/java/com/Bk24Shop/Shop/dto/ReceiptDTO.java
@@ -0,0 +1,18 @@
+package com.Bk24Shop.Shop.dto;
+
+import com.Bk24Shop.Shop.entity.Order;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ReceiptDTO {
+    
+    @OneToOne
+    private Order order;
+
+    private LocalDateTime timestamp;
+
+    // Getters and setters
+}

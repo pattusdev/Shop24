2acaff5c2a57c1f6249e12a95d840e7dc1ed1dd3
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-06 21:10:49 +0200

    Update DrinkService.java

--- src/main/java/com/Bk24Shop/Shop/Service/DrinkService.java
+++ src/main/java/com/Bk24Shop/Shop/Service/DrinkService.java
@@ -1,15 +1,16 @@
 package com.Bk24Shop.Shop.service;
 
+import com.Bk24Shop.Shop.dto.DrinkDTO;
 import com.Bk24Shop.Shop.entity.Drink;
 
 import java.util.HashMap;
 import java.util.List;
 
 public interface DrinkService {
-    HashMap<String, Object> createDrink(Drink drink);
+    HashMap<String, Object> createDrink(DrinkDTO drink);
     HashMap<String, Object> getAllDrinks();
     HashMap<String, Object> getDrinkById(Long id);
-    HashMap<String, Object> deleteDrink(Long drinkId);
+    HashMap<String, Object> deleteDrink(Long id);
     List getMostConsumedDrinks();
 }
 

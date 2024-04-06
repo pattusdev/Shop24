fb326e29c27541ca4e68d539fa8539c9b38e2ee2
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-06 21:10:57 +0200

    Update OrderItemService.java

--- src/main/java/com/Bk24Shop/Shop/service/OrderItemService.java
+++ src/main/java/com/Bk24Shop/Shop/service/OrderItemService.java
@@ -1,12 +1,13 @@
 package com.Bk24Shop.Shop.service;
 
 
+import com.Bk24Shop.Shop.dto.OrderItemDTO;
 import com.Bk24Shop.Shop.entity.OrderItem;
 
 import java.util.HashMap;
 
 public interface OrderItemService {
-    HashMap<String, Object> createOrderItem(OrderItem orderItem);
+    HashMap<String, Object> createOrderItem(OrderItemDTO orderItem);
     HashMap<String, Object> getAllOrderItems();
     HashMap<String, Object> getOrderItemById(Long orderId, Long drinkId);
     HashMap<String, Object> deleteOrderItem(Long orderId, Long drinkId);

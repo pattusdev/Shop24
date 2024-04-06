16cb6d5871fe7d7dadde2ee519dc7fc1fbbb8369
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-06 21:10:36 +0200

    Update CargoService.java

--- src/main/java/com/Bk24Shop/Shop/Service/CargoService.java
+++ src/main/java/com/Bk24Shop/Shop/Service/CargoService.java
@@ -1,14 +1,17 @@
 package com.Bk24Shop.Shop.service;
 
 
+import com.Bk24Shop.Shop.dto.CargoDTO;
 import com.Bk24Shop.Shop.entity.Cargo;
 
 import java.util.HashMap;
 
 public interface CargoService {
 
-    HashMap<String, Object> createCargo(Cargo cargo);
+    HashMap<String, Object> createCargo(CargoDTO cargo);
     HashMap<String, Object> getAllCargo();
-    HashMap<String, Object> getCargoById(Long cargoId);
-    HashMap<String, Object> deleteCargo(Long cargoId);
+    HashMap<String, Object> getCargoById(Long id);
+    HashMap<String, Object> deleteCargo(Long id);
+    HashMap<String, Object> findNearestCargoCompany(double latitude, double longitude);
+
 }

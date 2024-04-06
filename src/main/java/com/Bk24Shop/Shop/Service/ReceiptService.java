8a077c5b841a0e6f23953f7f5e587507a3ad0cc5
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-06 21:11:12 +0200

    Update ReceiptService.java

--- src/main/java/com/Bk24Shop/Shop/Service/ReceiptService.java
+++ src/main/java/com/Bk24Shop/Shop/Service/ReceiptService.java
@@ -1,11 +1,12 @@
 package com.Bk24Shop.Shop.service;
 
 
+import com.Bk24Shop.Shop.dto.ReceiptDTO;
 import com.Bk24Shop.Shop.entity.Receipt;
 import java.util.HashMap;
 
 public interface ReceiptService {
-    HashMap<String, Object> createReceipt(Receipt receipt);
+    HashMap<String, Object> createReceipt(ReceiptDTO receipt);
     HashMap<String, Object> getAllReceipts();
     HashMap<String, Object> getReceiptById(Long id);
     HashMap<String, Object> deleteReceipt(Long id);

b451f1b5e846e10289f1dcf95ad5ddf9b404b02d
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-12 16:42:53 +0200

    Update ShopApplication.java

--- src/main/java/com/Bk24Shop/Shop/ShopApplication.java
+++ src/main/java/com/Bk24Shop/Shop/ShopApplication.java
@@ -1,7 +1,9 @@
 package com.Bk24Shop.Shop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
 @SpringBootApplication
 public class ShopApplication {

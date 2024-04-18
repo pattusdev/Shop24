b3243d829b7383612519337a6a3220e2b6686ac5
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-18 11:35:04 +0200

    Update ShopApplication.java

--- src/main/java/com/Bk24Shop/Shop/ShopApplication.java
+++ src/main/java/com/Bk24Shop/Shop/ShopApplication.java
@@ -1,9 +1,14 @@
 package com.Bk24Shop.Shop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShopApplication {
@@ -11,5 +16,7 @@ public class ShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
+	
+
 
 }
